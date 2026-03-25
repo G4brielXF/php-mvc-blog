4101a2583923ec8f510ad3a6b17ba523cdb0de9d
G4brielXF <xavierdefreitasgabriel@gmail.com>
2026-03-25 08:06:37 -0300

    estou testando classe Upload

--- index.php
+++ index.php
@@ -3,4 +3,12 @@
 //Arquivo index responsável pela inicialização do sistema
 require 'vendor/autoload.php';
 
-require 'rotas.php';
+//require 'rotas.php';
+use sistema\Biblioteca\Upload;
+
+$upload = new Upload('upload');
+
+if(!empty($arquivo = $_FILES)){
+    $arquivo = $_FILES['arquivo'];
+    $upload->arquivo($arquivo, 'textos');
+}
